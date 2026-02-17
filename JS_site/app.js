3a848d4966dd8c7f785a415753c3efff5b64b15a
Maeva Pereira <maeva.pereira@le-campus-numerique.fr>
2026-02-17 09:46:42 +0100

    Menu dropdown

--- JS_site/app.js
+++ JS_site/app.js
@@ -1,52 +1,62 @@
 //base de données local
 
-const articles = [
-    { id: 1, titre: "Lune", contenu: " text descriptif trip 1", image: "media/moon_title.jpg" },
-    { id: 2, titre: "Mars", contenu: " text descriptif trip 2", image: "media/mars.jpg" },
-    { id: 3, titre: "Jupiter", contenu: " text descriptif trip 3", image: "media/jupiter.jpg" }
+const
+    articles = [
+    { id: 1, titre: "Pandora", contenu: " Planète peuplé de Na'vi, ils aiment communier avec la nature ", image: "pictures/pandora.jpg" },
+    { id: 2, titre: "Arakis", contenu: " des épices et des vers géants ", image: "pictures/arakis.jpg" },
+    { id: 3, titre: "Endor", contenu: " des ewoks un peu rageux peuvent être trouvés dans la forêt ", image: "pictures/endor.jpg" }
 ];
 
-function createCard(article) {
-    const divProduit = document.createElement("div");
-    divProduit.setAttribute("class", "trip");
 
-    const divImage = document.createElement("div"); //on créer la div
-    divImage.setAttribute("class", "photo"); //on lui met les attributs
-    divProduit.appendChild(divImage); //divImage enfant de divProduit
+function createCard(article, container) {
 
-    const p = document.createElement("p");
-    divImage.appendChild(p);
+    const trip = document.createElement("div");
+    trip.classList.add("trip");
+    container.appendChild(trip);
 
-    const image = document.createElement("img");
-    image.setAttribute("src", article.image);
-    p.appendChild(image);
+        const photo_trip = document.createElement("div");
+        photo_trip.classList.add("photo_trip");
+        trip.appendChild(photo_trip);
 
-    const divText = document.createElement("div");
-    divText.setAttribute("class", "text_trip");
-    divProduit.appendChild(divText);
+            const img = document.createElement("img");
+            photo_trip.classList.add("img");
+            photo_trip.appendChild(img);
+            img.src = article.image;
 
-    const h1 = document.createElement("h1");
-    divText.appendChild(h1);
-    h1.textContent = article.titre;
 
-    const textProduit = document.createElement("p");
-    textProduit.setAttribute("class", "text_produit");
-    divText.appendChild(textProduit);
-    textProduit.textContent = article.contenu;
+        const texte_trip = document.createElement("div");
+        texte_trip.classList.add("texte_trip");
+        trip.appendChild(texte_trip);
 
-    return divProduit;
+            const h1 = document.createElement("h1");
+            texte_trip.classList.add("h1");
+            texte_trip.appendChild(h1);
+            h1.textContent = article.titre;
+
+            const p = document.createElement("p");
+            texte_trip.classList.add("p");
+            texte_trip.appendChild(p);
+            p.textContent = article.contenu;
+
 }
 
+window.addEventListener('load', function () {
+    const container = document.getElementById("container");
+    articles.forEach((article) => { createCard(article, container) });
+});
 
-let x;
-const container = container.querySelector("#feed-container"); //creation de variable container qui contient la div feed-container
-for (let i = 0; i < articles.length; i++) { //boucle des articles
-    x = createCard(articles[i]);
-    container.appendChild(x);
+    async function fetchArticles() {
+        const reponse = await fetch("https://v2.jokeapi.dev/joke/Any?lang=fr&amount=10");
+        const jokeapi = await reponse.json();
+        console.log(jokeapi);
+    }
+
+
+//dropdown
+function myFunction() {
+        //alert() pour vérifier si le bouton fonctionne
+    document.getElementById("myDropdown").classList.toggle("show");
 }
-
-
-
 
 
 
